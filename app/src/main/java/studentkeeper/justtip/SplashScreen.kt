ad35e9dabd75ff674185ae0626d2f1158f530919
Felix Cahyadi <cahyadi.felix@gmail.com>
2018-07-29 21:53:25 +0700

    Initial commit

---
+++ app/src/main/java/studentkeeper/justtip/SplashScreen.kt
@@ -0,0 +1,30 @@
+package studentkeeper.justtip
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        // Example of a call to a native method
+        sample_text.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
